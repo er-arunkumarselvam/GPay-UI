2c0c54486ea210b93a46ecb80fc961e09b6c6d70
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2023-10-09 17:42:51 +0530

    design footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      xs: ["11px", "16px"],
+      sm: ["14px", "18px"],
+      normal: ["16px", "20px"],
+      base: ["18px", "20px"],
+      lg: ["20px", "25px"],
+    },
     extend: {},
   },
   plugins: [],
